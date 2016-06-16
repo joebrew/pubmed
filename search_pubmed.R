239dacb6225d9dc36082f568ea1d4980f2371de4
Joe Brew <joebrew@gmail.com>
2016-06-16 14:50:52 +0200

    rosauros search

--- search_pubmed.R
+++ search_pubmed.R
@@ -54,87 +54,90 @@ pubmed <- function(start_year = 2014,
     for (year in 1:length(years)){
       message(paste0('Fetching records for year ', years[year]))
       
-      # Perform search
-      search_query <- 
-        EUtilsSummary(search_topic, 
-                      db="pubmed", 
-                      mindate=years[year], 
-                      maxdate=years[year],
-                      retmax = 5000)
-      
-      # See results summary
-      # summary(search_query)
-      
-      # See number of results
-      # QueryCount(search_query)
-      
-      # Get IDs of the articles returned in our query
-      # Qids <- queryId(search_query)
-      
-      # Actually fetch data
-      records <- EUtilsGet(search_query)
-      
-      # Turn into a data.frame
-      pubmed_data <- 
-        data.frame('title' = ArticleTitle(records),
-                   # 'Abstract' = AbstractText(records),
-                   'language' = Language(records),
-                   'country' = Country(records),
-                   'id' = ArticleId(records),
-                   'year' = years[year],
-                   'month' = MonthPubmed(records),
-                   'day' = DayPubmed(records),
-                   'affiliation' = Affiliation(records))
-      # Cited(records)
-      # RefSource(records)
-
-      # Create separate dataframe for abstracts
-      abstracts <- data.frame('id' = ArticleId(records),
-                              'abstract' = AbstractText(records))
-      
-      # Add authors separately
-      temp <- Author(records)
-      first_authors <- lapply(temp,
-                              function(x){
-                                x[1,]
-                              })
-      last_authors <- lapply(temp,
-                             function(x){
-                               x[nrow(x),]
-                             })
-      for (i in c('first', 'last')){
-        # Last name
-        pubmed_data[,paste(i, '_author_last_name')] <- 
-          unlist(lapply(get(paste0(i, '_authors')), function(x){x['LastName']}))
+      try({
+        # Perform search
+        search_query <- 
+          EUtilsSummary(search_topic, 
+                        db="pubmed", 
+                        mindate=years[year], 
+                        maxdate=years[year],
+                        retmax = 5000)
         
-        # First name
-        pubmed_data[,paste(i, '_author_first_name')] <- 
-          unlist(lapply(get(paste0(i, '_authors')), function(x){x['ForeName']}))
+        # See results summary
+        # summary(search_query)
         
-        # Initials
-        pubmed_data[,paste(i, '_author_initials')] <- 
-          unlist(lapply(get(paste0(i, '_authors')), function(x){x['Initials']}))
-      }
+        # See number of results
+        # QueryCount(search_query)
+        
+        # Get IDs of the articles returned in our query
+        # Qids <- queryId(search_query)
+        
+        # Actually fetch data
+        records <- EUtilsGet(search_query)
+        
+        # Turn into a data.frame
+        pubmed_data <- 
+          data.frame('title' = ArticleTitle(records),
+                     # 'Abstract' = AbstractText(records),
+                     'language' = Language(records),
+                     'country' = Country(records),
+                     'id' = ArticleId(records),
+                     'year' = years[year],
+                     'month' = MonthPubmed(records),
+                     'day' = DayPubmed(records),
+                     'affiliation' = Affiliation(records))
+        # Cited(records)
+        # RefSource(records)
+        
+        # Create separate dataframe for abstracts
+        abstracts <- data.frame('id' = ArticleId(records),
+                                'abstract' = AbstractText(records))
+        
+        # Add authors separately
+        temp <- Author(records)
+        first_authors <- lapply(temp,
+                                function(x){
+                                  x[1,]
+                                })
+        last_authors <- lapply(temp,
+                               function(x){
+                                 x[nrow(x),]
+                               })
+        for (i in c('first', 'last')){
+          # Last name
+          pubmed_data[,paste(i, '_author_last_name')] <- 
+            unlist(lapply(get(paste0(i, '_authors')), function(x){x['LastName']}))
+          
+          # First name
+          pubmed_data[,paste(i, '_author_first_name')] <- 
+            unlist(lapply(get(paste0(i, '_authors')), function(x){x['ForeName']}))
+          
+          # Initials
+          pubmed_data[,paste(i, '_author_initials')] <- 
+            unlist(lapply(get(paste0(i, '_authors')), function(x){x['Initials']}))
+        }
+        
+        # All authors
+        pubmed_data$all_authors <- 
+          unlist(lapply(Author(records), function(x){
+            paste0(x$ForeName, 
+                   ' ', 
+                   x$LastName, 
+                   collapse = ', ')}))
+        
+        # Add the year
+        pubmed_data$year <- years[year]
+        
+        # Add results to results_list
+        results_list[[year]] <- pubmed_data
+        
+        # Add abstract to abstracts list
+        abstracts_list[[year]] <- abstracts
+        
+        # Remove unecessary objects
+        rm(pubmed_data, abstracts)
+      })
       
-      # All authors
-      pubmed_data$all_authors <- 
-        unlist(lapply(Author(records), function(x){
-          paste0(x$ForeName, 
-                 ' ', 
-                 x$LastName, 
-                 collapse = ', ')}))
-      
-      # Add the year
-      pubmed_data$year <- years[year]
-      
-      # Add results to results_list
-      results_list[[year]] <- pubmed_data
-      
-      # Add abstract to abstracts list
-      abstracts_list[[year]] <- abstracts
-      
-      # Remove unecessary objects
-      rm(pubmed_data, abstracts)
     }
     
     # Bind together the results
